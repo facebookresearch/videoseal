6c8fd604fc7b2b53c41d6caf1cde2e9ae757dc03
Pierre Fernandez <pfz@fb.com>
2024-12-11 10:21:18 -0800

    updts

--- videoseal/losses/perceptual.py
+++ videoseal/losses/perceptual.py
@@ -15,9 +15,46 @@ from .focal import FocalFrequencyLoss
 from .ssim import SSIM, MSSSIM
 from .yuvloss import YUVLoss
 
+
+def build_loss(loss_name):
+    if loss_name == "none":
+        return NoneLoss()
+    elif loss_name == "lpips":
+        return LPIPS(net="vgg").eval()
+    elif loss_name == "mse":
+        return nn.MSELoss()
+    elif loss_name == "yuv":
+        return YUVLoss()
+    elif loss_name == "focal":
+        return FocalFrequencyLoss()
+    elif loss_name == "ssim":
+        return SSIM()
+    elif loss_name == "msssim":
+        return MSSSIM()
+    elif loss_name == "jnd":
+        return JNDLoss(loss_type=0)
+    elif loss_name == "jnd2":
+        return JNDLoss(loss_type=2)
+    elif loss_name == "dists":
+        return DISTS("/path/to/dists_ckpt.pth").eval()
+    elif loss_name == "watson_vgg":
+        model = WatsonDistanceVgg(reduction="none")
+        ckpt_loss = "/path/to/rgb_watson_vgg_trial0.pth"
+        model.load_state_dict(torch.load(ckpt_loss))
+        return model
+    elif loss_name == "watson_dft":
+        model = ColorWrapper(WatsonDistanceFft, (), {"reduction": "none"})
+        ckpt_loss = "/path/to/rgb_watson_fft_trial0.pth"
+        model.load_state_dict(torch.load(ckpt_loss))
+        return model
+    else:
+        raise ValueError(f"Loss type {loss_name} not supported.")
+    
+
 class NoneLoss(nn.Module):
     def forward(self, x, y):
         return torch.zeros(1, requires_grad=True)
+
 
 class PerceptualLoss(nn.Module):
     def __init__(
@@ -26,35 +63,8 @@ class PerceptualLoss(nn.Module):
     ):
         super(PerceptualLoss, self).__init__()
 
-        self.losses = {
-            "none": NoneLoss(),
-            "lpips": LPIPS(net="vgg").eval(),
-            "mse": nn.MSELoss(),
-            "yuv": YUVLoss(),
-            "jnd2": JNDLoss(loss_type=2),
-            "focal": FocalFrequencyLoss(),
-            "ssim": SSIM(),
-            "msssim": MSSSIM(),
-            "jnd": JNDLoss(loss_type=0),
-            # "dists": DISTS("/checkpoint/pfz/projects/ai_signature/loss_weights/dists_ckpt.pth").eval(),
-            # "watson_vgg": self.load_watson_vgg(),
-            # "watson_dft": self.load_watson_dft(),
-        }
-
         self.percep_loss = percep_loss
         self.perceptual_loss = self.create_perceptual_loss(percep_loss)
-
-    def load_watson_vgg(self):
-        model = WatsonDistanceVgg(reduction="none")
-        ckpt_loss = "/checkpoint/pfz/projects/ai_signature/loss_weights/rgb_watson_vgg_trial0.pth"
-        model.load_state_dict(torch.load(ckpt_loss))
-        return model
-
-    def load_watson_dft(self):
-        model = ColorWrapper(WatsonDistanceFft, (), {"reduction": "none"})
-        ckpt_loss = "/checkpoint/pfz/projects/ai_signature/loss_weights/rgb_watson_fft_trial0.pth"
-        model.load_state_dict(torch.load(ckpt_loss))
-        return model
 
     def create_perceptual_loss(
         self, 
@@ -66,10 +76,24 @@ class PerceptualLoss(nn.Module):
             percep_loss: (str) The perceptual loss string.
                 Example: "lpips", "lpips+mse", "lpips+0.1_mse", ...
         """
+        # split the string into the different losses
         parts = percep_loss.split('+')
-        if len(parts) == 1 and parts[0] in self.losses:
-            return self.losses[parts[0]]
+
+        # only one loss
+        if len(parts) == 1:
+            loss = parts[0]
+            return build_loss(loss)
         
+        # several losses
+        self.losses = {}
+        for part in parts:
+            if '_' in part:  # Check if the format is 'weight_loss'
+                weight, loss_key = part.split('_')
+            else:
+                weight, loss_key = 1, part
+            self.losses[loss_key] = build_loss(loss_key)
+        
+        # create the combined loss function
         def combined_loss(x, y):
             total_loss = 0
             for part in parts:
@@ -78,10 +102,7 @@ class PerceptualLoss(nn.Module):
                 else:
                     weight, loss_key = 1, part
                 weight = float(weight)
-                if loss_key in self.losses:
-                    total_loss += weight * self.losses[loss_key](x, y).mean()
-                else:
-                    raise ValueError(f"Loss type {loss_key} not supported.")
+                total_loss += weight * self.losses[loss_key](x, y).mean()
             return total_loss
         
         return combined_loss
--- videoseal/losses/videosealloss.py
+++ videoseal/losses/videosealloss.py
@@ -9,28 +9,26 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from lpips import LPIPS
 
 from ..modules.discriminator import NLayerDiscriminator
-from .dists import DISTS
-from .jndloss import JNDLoss
 from .perceptual import PerceptualLoss
-from .watson_fft import ColorWrapper, WatsonDistanceFft
-from .watson_vgg import WatsonDistanceVgg
-
 
 def hinge_d_loss(logits_real, logits_fake):
+    """
+    https://paperswithcode.com/method/gan-hinge-loss
+    """
     loss_real = torch.mean(F.relu(1. - logits_real))
     loss_fake = torch.mean(F.relu(1. + logits_fake))
     d_loss = 0.5 * (loss_real + loss_fake)
     return d_loss
 
-
 def adopt_weight(weight, global_step, threshold=0, value=0.):
+    """
+    Adopt weight if global step is less than threshold
+    """
     if global_step < threshold:
         weight = value
     return weight
-
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -218,11 +216,6 @@ class VideosealLoss(nn.Module):
             return total_loss, log
 
         if optimizer_idx == 1:  # discriminator update
-
-            # training only embedder in this case the
-            # gan like discriminator (disc) should be in train mode
-            # self.discriminator.train()
-
             if cond is None:
                 # detach here prevents gradient leakage to any module other than the discriminator
                 logits_real = self.discriminator(inputs.contiguous().detach())
