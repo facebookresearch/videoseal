4b7f790737d566745ddaf88a742a7681bc4272c7
alexmourachko <alexmourachko@fb.com>
2024-12-12 01:41:13 +0000

    adding reqs for combined av wm

--- inference_av.py
+++ inference_av.py
@@ -8,11 +8,25 @@ from audioseal import AudioSeal
 import videoseal
 from videoseal.utils.display import save_video_audio_to_mp4
 
-parser = argparse.ArgumentParser(description='Video and Audio Watermarking')
-parser.add_argument('--input_file', type=str, required=True, help='Path to the input mp4 file')
-parser.add_argument('--output_file', type=str, required=False, default="watermarked.mp4", help='Path to save the watermarked mp4 file')
-parser.add_argument('--video_only', action='store_true', help='Watermark only the video, not the audio')
-parser.add_argument('--detect', action='store_true', help='Detect watermarks in the output video and audio')
+parser = argparse.ArgumentParser(description="Video and Audio Watermarking")
+parser.add_argument(
+    "--input_file", type=str, required=True, help="Path to the input mp4 file"
+)
+parser.add_argument(
+    "--output_file",
+    type=str,
+    required=False,
+    default="watermarked.mp4",
+    help="Path to save the watermarked mp4 file",
+)
+parser.add_argument(
+    "--video_only", action="store_true", help="Watermark only the video, not the audio"
+)
+parser.add_argument(
+    "--detect",
+    action="store_true",
+    help="Detect watermarks in the output video and audio",
+)
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -24,8 +38,10 @@ video_model.to(device)
 # Read the video and convert to tensor format
 video, audio, info = torchvision.io.read_video(args.input_file, output_format="TCHW")
 
-fps = info['video_fps']
-sample_rate = info['audio_fps']
+assert "audio_fps" in info, "The input video must contain an audio track. Simply refer to the main videoseal inference code if not."
+
+fps = info["video_fps"]
+sample_rate = info["audio_fps"]
 
 # Normalize the video frames to the range [0, 1] and trim to 1 second
 audio = audio.float()
@@ -34,14 +50,13 @@ video = video.float() / 255.0
 
 if not args.detect:
 
-
     # Perform watermark embedding on video
     with torch.no_grad():
         outputs = video_model.embed(video, is_video=True)
 
     # Extract the results
     video_w = outputs["imgs_w"]  # Watermarked video frames
-    video_msgs = outputs["msgs"]      # Watermark messages
+    video_msgs = outputs["msgs"]  # Watermark messages
 
     if not args.video_only:
         # Resample the audio to 16kHz for watermarking
@@ -61,8 +76,15 @@ if not args.detect:
 
         # Get the watermark for the audio
         with torch.no_grad():
-            audio_msg = torch.randint(0, 2, (audio_16k_mono_batched.shape[0], audio_model.msg_processor.nbits), device=audio_16k_mono_batched.device)
-            watermark = audio_model.get_watermark(audio_16k_mono_batched, 16000, message=audio_msg)
+            audio_msg = torch.randint(
+                0,
+                2,
+                (audio_16k_mono_batched.shape[0], audio_model.msg_processor.nbits),
+                device=audio_16k_mono_batched.device,
+            )
+            watermark = audio_model.get_watermark(
+                audio_16k_mono_batched, 16000, message=audio_msg
+            )
 
         # Embed the watermark in the audio
         audio_16k_w = audio_16k_mono_batched + watermark
@@ -82,11 +104,11 @@ if not args.detect:
     # Save the watermarked video and audio
 
     save_video_audio_to_mp4(
-        video_tensor=video, 
-        audio_tensor=audio_w,     
-        fps=int(fps), 
+        video_tensor=video,
+        audio_tensor=audio_w,
+        fps=int(fps),
         audio_sample_rate=int(sample_rate),
-        output_filename=args.output_file
+        output_filename=args.output_file,
     )
 
     print(f"encoded message: \n Audio: {audio_msg} \n Video {video_msgs[0]}")
@@ -99,17 +121,19 @@ else:
 
     if not args.video_only:
         if len(audio.shape) == 2:
-            audio = audio.unsqueeze(0)# batchify 
+            audio = audio.unsqueeze(0)  # batchify
 
         # if stereo convert to mono
         if audio.shape[1] > 1:
             audio = torch.mean(audio, dim=1, keepdim=True)
 
-        # Load the AudioSeal detector model        
+        # Load the AudioSeal detector model
         detector = AudioSeal.load_detector("audioseal_detector_16bits")
 
         # Detect watermarks in the audio
         with torch.no_grad():
-            result, message = detector.detect_watermark(torchaudio.transforms.Resample(sample_rate, 16000)(audio), 16000)
+            result, message = detector.detect_watermark(
+                torchaudio.transforms.Resample(sample_rate, 16000)(audio), 16000
+            )
         print(f"Detection result for audio: {result}")
         print(f"Extracted message from audio: {message}")
