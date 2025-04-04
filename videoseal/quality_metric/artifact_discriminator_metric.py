06ebfbd4f4730e559c4da586c23fc9345020c2b7
Tomas Soucek <soucek@meta.com>
2025-04-04 09:54:30 +0000

    support image sizes other than 256x256

--- videoseal/quality_metric/artifact_discriminator_metric.py
+++ videoseal/quality_metric/artifact_discriminator_metric.py
@@ -56,12 +56,31 @@ class ArtifactDiscriminatorLoss(torch.nn.Module):
     def forward(self, imgs_w: torch.Tensor, imgs_ori=None) -> torch.Tensor:
         # if `imgs_ori`` is None, the loss is computed only with respect to the watermarked images, maximizing the discriminator score.
         # if `imgs_ori` is provided, it computes the ranking loss (the loss used to train this model).
-        assert imgs_w.dim() == 4 and imgs_w.shape[1] == 3 and imgs_w.dtype == torch.float32, "Input tensor must be 4D and RGB (B, 3, H, W) with values in [0, 1]." 
+        assert imgs_w.dim() == 4 and imgs_w.shape[1] == 3 and imgs_w.dtype == torch.float32, "Input tensor must be 4D and RGB (B, 3, H, W) with values in [0, 1]."
+
+        _, _, H, W = imgs_w.shape
+        assert H >= 256 and W >= 256, "ArtifactDiscriminatorLoss has been trained on 256x256 patches, input images should be at least 256x256."
+        if H > 256 or W > 256:
+            # compute the loss on the four corner crops and the center crop of the input images
+            top_left = imgs_w[:, :, :256, :256]
+            top_right = imgs_w[:, :, :256, -256:]
+            bottom_left = imgs_w[:, :, -256:, :256]
+            bottom_right = imgs_w[:, :, -256:, -256:]
+            center = imgs_w[:, :, (H-256)//2:(H+256)//2, (W-256)//2:(W+256)//2]
+            imgs_w = torch.cat([top_left, top_right, bottom_left, bottom_right, center], dim=0)
+
+            if imgs_ori is not None:
+                top_left_ori = imgs_ori[:, :, :256, :256]
+                top_right_ori = imgs_ori[:, :, :256, -256:]
+                bottom_left_ori = imgs_ori[:, :, -256:, :256]
+                bottom_right_ori = imgs_ori[:, :, -256:, -256:]
+                center_ori = imgs_ori[:, :, (H-256)//2:(H+256)//2, (W-256)//2:(W+256)//2]
+                imgs_ori = torch.cat([top_left_ori, top_right_ori, bottom_left_ori, bottom_right_ori, center_ori], dim=0)
 
         if imgs_ori is not None:
             wm_logits = self.model(imgs_w)
             real_logits = self.model(imgs_ori)
-            loss = F.binary_cross_entropy_with_logits(real_logits - wm_logits, torch.ones_like(real_logits)).mean()
+            loss = torch.nn.functional.binary_cross_entropy_with_logits(real_logits - wm_logits, torch.ones_like(real_logits)).mean()
         else:
             loss = -self.model(imgs_w).mean()
         return loss
